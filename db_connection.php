9cf742f1eca20e855b81645d2701f5e74458e791
Jordan <jordanmckee@outlook.com>
2022-10-27 15:38:37 -0400

    fix spacing on db_conn

--- db_connection.php
+++ db_connection.php
@@ -2,19 +2,19 @@
 
 function OpenCon()
  {
- $dbhost = "127.0.0.1";
- $dbuser = "root";
- $dbpass = "";
- $db = "polymer_selectordb";
+    $dbhost = "127.0.0.1";
+    $dbuser = "root";
+    $dbpass = "";
+    $db = "polymer_selectordb";
 
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
- 
- return $conn;
+    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+    
+    return $conn;
  }
  
 function CloseCon($conn)
  {
- $conn -> close();
+    $conn -> close();
  }
    
 ?>
